8066e0a5475c1b02644eb8a0eb45d773dd4dd13f
pramodmax <pramodmax@gmail.com>
2024-10-02 09:16:10 +1000

    updated the filepoller class

--- src/main/java/com/redhat/demo/FilePollerRoute.java
+++ src/main/java/com/redhat/demo/FilePollerRoute.java
@@ -1,26 +1,5 @@
 package com.redhat.demo;
-//
-//import org.apache.camel.builder.RouteBuilder;
-//import org.springframework.stereotype.Component;
-//
-////@Component
-//public class FilePollerRoute extends RouteBuilder {
-//
-//    @Override
-//    public void configure() throws Exception {
-////        from("file://{{input.folder}}?noop=true")
-//        from("file:///Volumes/Extra-hdd/MUFG/camel-route-web-demo/src-file-folder?noop=true")
-//                .log("Processing file: ${header.CamelFileName}")
-//                .process(exchange -> {
-//                    String fileContent = exchange.getIn().getBody(String.class);
-//                    System.out.println("File Content: " + fileContent);
-//                });
-//    }
-//}
-
-
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.PropertyInject;
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
